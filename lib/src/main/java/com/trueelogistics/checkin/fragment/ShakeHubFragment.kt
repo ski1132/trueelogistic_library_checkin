cc6d838461d1f9efb5c9fc4430a403e140ea23a6
Ski_True <thanadon_sri@true-e-logistics.com>
2019-10-12 22:17:51 +0700

    fix showDialog Checkin in Shake Checkin

--- lib/src/main/java/com/trueelogistics/checkin/fragment/ShakeHubFragment.kt
+++ lib/src/main/java/com/trueelogistics/checkin/fragment/ShakeHubFragment.kt
@@ -33,6 +33,7 @@ class ShakeHubFragment : Fragment(), OnClickItemCallback {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        shakeDialog = CheckInDialogFragment()
         back_page.setOnClickListener {
             activity?.onBackPressed()
         }
